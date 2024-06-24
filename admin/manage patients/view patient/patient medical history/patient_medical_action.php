5a84bf02d95430ec4060e719bf01760615c3e4f7
hadifarhat2001 <farhathadi02@gmail.com>
2024-06-24 10:39:39 +0300

    Update patient_medical_action.php

--- admin/manage patients/view patient/patient medical history/patient_medical_action.php
+++ admin/manage patients/view patient/patient medical history/patient_medical_action.php
@@ -3,17 +3,17 @@ require_once '../../../../connection/db_connection.php';
 
 session_start();
 if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
-    header("Location: login.php"); 
+    header("Location: login.php");
     exit();
 }
 
 $PatientID = $_SESSION['PatientID'];
 $username = $_SESSION['username'];
 
-$sql = "SELECT a.MedicineHistoryID, d.MedicineTypeName, a.Dosage, a.PrescriptionDate
-        FROM medicinehistory a
-        INNER JOIN medicinetypes d ON a.MedicineTypeID = d.MedicineTypeID 
-        WHERE a.PatientID = ? ";
+$sql = "SELECT a.PrescriptionID,CONCAT('Dr. ', d.FirstName, ' ', d.LastName) AS DoctorName , a.Medication, a.Dosage, a.PrescriptionDate
+        FROM prescriptions a
+        INNER JOIN doctors d ON a.DoctorID = d.DoctorID 
+        WHERE a.PatientID = ?";
 
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("i", $PatientID);
@@ -21,7 +21,7 @@ $stmt->execute();
 $result = $stmt->get_result();
 $medicalHistory = $result->fetch_all(MYSQLI_ASSOC);
 
- json_encode($medicalHistory);
+json_encode($medicalHistory);
 
 
 ?>
